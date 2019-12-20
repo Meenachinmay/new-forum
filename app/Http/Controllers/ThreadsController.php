2cc69b813573dcaf400656562db52d0d5b7acc7a
Chinmay Anand <chinmayanand896@gmail.com>
2019-12-20 18:04:41 +0530

    initial commit

---
+++ app/Http/Controllers/ThreadsController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Thread;
+use Illuminate\Http\Request;
+
+class ThreadsController extends Controller
+{
+
+    public function index(){
+        $threads = Thread::all();
+        return view('thread.index', compact('threads'));
+    }
+
+    public function show(Thread $thread){
+        $thread = Thread::where('id', $thread->id)->firstOrFail();
+        return view('thread.show', compact('thread'));
+    }
+}
---
+++ resources/views/thread/index.blade.php
@@ -0,0 +1,24 @@
+@extends('layouts.app')
+@section('content')
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">Threads on forum</div>
+                    <div class="card-body">
+                        @foreach($threads as $thread)
+                            <div class="card mb-3">
+                                <div class="card-header">
+                                    <a href="{{ $thread->path() }}">{{ $thread->title }}</a>
+                                </div>
+                                <div class="card-body">
+                                    {{ $thread->body }}
+                                </div>
+                            </div>
+                        @endforeach
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
